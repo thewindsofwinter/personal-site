0e9f3ee4ae099cc5398a6e65b36d11f08f679b80
Andy Tang <winterwind2022@gmail.com>
2022-12-27 13:10:10 -0600

    make reasonable star distribution

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -1,3 +1,5 @@
+import { colorTemperature2rgb } from 'color-temperature'
+
 export default function sketch(p){
     let canvas;
     let stars;
@@ -13,7 +15,8 @@ export default function sketch(p){
       while(sizeBracket > 1) {
         let createdCount = 0;
         while(createdCount < bracketCount) {
-          stars.push(new Star(p, sizeBracket));
+          let temp = p.random(6250000, 900000000);
+          stars.push(new Star(p, sizeBracket, temp));
           createdCount++;
         }
 
@@ -41,37 +44,12 @@ export default function sketch(p){
     }
 }
 
-function hslToRgb(h, s, l){
-  var r, g, b;
-
-  if(s == 0){
-      r = g = b = l; // achromatic
-  }else{
-      var hue2rgb = function hue2rgb(p, q, t){
-          if(t < 0) t += 1;
-          if(t > 1) t -= 1;
-          if(t < 1/6) return p + (q - p) * 6 * t;
-          if(t < 1/2) return q;
-          if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-          return p;
-      }
-
-      var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-      var p = 2 * l - q;
-      r = hue2rgb(p, q, h + 1/3);
-      g = hue2rgb(p, q, h);
-      b = hue2rgb(p, q, h - 1/3);
-  }
-
-  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-}
-
 class Star {
-  constructor(p, size) {
+  constructor(p, size, temp) {
     this.diameter = size;
     this.x = p.random(size + 10, p.windowWidth - size - 10);
     this.y = p.random(size + 10, p.windowHeight - size - 10);
-    this.rgb = hslToRgb(p.random(1), 1, 0.8);
+    this.rgb = colorTemperature2rgb(Math.sqrt(temp));
     this.opacity = 255;
     this.twinkleRate = p.random(-0.1, 0.1);
 
@@ -95,7 +73,7 @@ class Star {
 
   display(p) {
     p.noStroke();
-    p.fill(this.rgb[0], this.rgb[1], this.rgb[2], 255);
+    p.fill(this.rgb.red, this.rgb.green, this.rgb.blue, 255);
     p.ellipse(this.x, this.y, this.diameter, this.diameter);
   }
 }
