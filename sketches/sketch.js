9ff5ae0d1dbc91ea333e039eb6aaf4af0b7205e0
Andy Tang <winterwind2022@gmail.com>
2022-12-27 12:31:43 -0600

    more p5 stars

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -41,18 +41,42 @@ export default function sketch(p){
     }
 }
 
+function hslToRgb(h, s, l){
+  var r, g, b;
+
+  if(s == 0){
+      r = g = b = l; // achromatic
+  }else{
+      var hue2rgb = function hue2rgb(p, q, t){
+          if(t < 0) t += 1;
+          if(t > 1) t -= 1;
+          if(t < 1/6) return p + (q - p) * 6 * t;
+          if(t < 1/2) return q;
+          if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+          return p;
+      }
+
+      var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      var p = 2 * l - q;
+      r = hue2rgb(p, q, h + 1/3);
+      g = hue2rgb(p, q, h);
+      b = hue2rgb(p, q, h - 1/3);
+  }
+
+  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
 class Star {
   constructor(p, size) {
     this.diameter = size;
     this.x = p.random(size + 10, p.windowWidth - size - 10);
     this.y = p.random(size + 10, p.windowHeight - size - 10);
-    this.red = p.random(255);
-    this.green = p.random(255);
+    this.rgb = hslToRgb(p.random(1), 1, 0.8);
     this.opacity = 255;
     this.twinkleRate = p.random(-0.1, 0.1);
 
     console.log(this.x + " " + this.y + " " + this.twinkleRate);
-    console.log(this.color);
+    console.log(this.rgb);
   }
 
   twinkle() {
@@ -71,7 +95,7 @@ class Star {
 
   display(p) {
     p.noStroke();
-    p.fill(this.red, this.green, 0, this.opacity);
+    p.fill(this.rgb[0], this.rgb[1], this.rgb[2], 255);
     p.ellipse(this.x, this.y, this.diameter, this.diameter);
   }
 }
