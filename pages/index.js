e5348e9eb73901a6d4e288466090c7ad3d2bb1e4
Andy Tang <winterwind2022@gmail.com>
2022-12-27 10:56:43 -0600

    install p5 wrapper

--- pages/index.js
+++ pages/index.js
@@ -11,17 +11,39 @@ export default function Home() {
   
   return (
     <div className="bg-ink min-h-screen">
-      <canvas className="absolute top-0 left-0 w-screen h-screen" onLoad={() => { stars(document.getElementsByTagName("canvas")[0]); }}>
-      </canvas>
       <Head>
         <title>Andy Tang</title>
         <meta name="description" content="Software engineer, Stanford freshman, and stargazer. I seek to build tools which will empower others to dream." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
+      <StarryNight />
       <h1 className="text-3xl font-bold underline text-starlight">
         Hello world!
       </h1>
     </div>
   )
+}
+
+class StarryNight extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+     const img = new Image();
+      img.onload =() => {
+        // image  has been loaded
+        this.imagestore()
+      };
+
+      img.src = 'image_7.jpg';
+  }
+
+  render() {
+    return (
+      <canvas className="absolute top-0 left-0 w-screen h-screen" onLoad={() => { stars(document.getElementsByTagName("canvas")[0]); }}>
+      </canvas>
+    );
+  }
 }
