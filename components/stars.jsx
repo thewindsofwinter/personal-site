ff61c7027eff9fc690eb353656dddc303da6e17a
Andy Tang <winterwind2022@gmail.com>
2022-12-28 18:30:43 -0600

    why are all the stars white

--- components/stars.jsx
+++ components/stars.jsx
@@ -5,20 +5,95 @@ import * as THREE from 'three'
 import * as random from 'maath/random'
 import { colorTemperature2rgb } from 'color-temperature'
 
+/**
+ * Converts an RGB color value to HSL. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes r, g, and b are contained in the set [0, 255] and
+ * returns h, s, and l in the set [0, 1].
+ *
+ * @param   Number  r       The red color value
+ * @param   Number  g       The green color value
+ * @param   Number  b       The blue color value
+ * @return  Array           The HSL representation
+ */
+function rgbToHsl(r, g, b) {
+  r /= 255, g /= 255, b /= 255;
+
+  var max = Math.max(r, g, b), min = Math.min(r, g, b);
+  var h, s, l = (max + min) / 2;
+
+  if (max == min) {
+    h = s = 0; // achromatic
+  } else {
+    var d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+    switch (max) {
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
+    }
+
+    h /= 6;
+  }
+
+  return [ h, s, l ];
+}
+
+/**
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   Number  h       The hue
+ * @param   Number  s       The saturation
+ * @param   Number  l       The lightness
+ * @return  Array           The RGB representation
+ */
+function hslToRgb(h, s, l) {
+  var r, g, b;
+
+  if (s == 0) {
+    r = g = b = l; // achromatic
+  } else {
+    function hue2rgb(p, q, t) {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1/6) return p + (q - p) * 6 * t;
+      if (t < 1/2) return q;
+      if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+      return p;
+    }
+
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
+
+    r = hue2rgb(p, q, h + 1/3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1/3);
+  }
+
+  return [ r * 255, g * 255, b * 255 ];
+}
+
 // Create 10,000 stars, 
 function Stars(props) {
+  const numStars = 100;
   const ref = useRef()
-  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.5 }))
+  const [sphere] = useState(() => random.inSphere(new Float32Array(numStars), { radius: 1.5 }))
   const [colors] = useState(() => {
-    let colors = new Float32Array(15000);
+    let colors = new Float32Array(numStars * 3);
 
-    for(let i = 0; i < 15000; i+=3) {
-      let temperature = 2500 + Math.pow(Math.random(), 4) * (30000 - 2500); 
-      let rgb = colorTemperature2rgb(temperature);
+    for(let i = 0; i < numStars * 3; i += 3) {
+      let temperature = 2500 + Math.pow(Math.random(), 5) * (30000 - 2500); 
+      let tempRGB = colorTemperature2rgb(temperature);
+      let tempHSL = rgbToHsl(tempRGB.red, tempRGB.green, tempRGB.blue);
+      let rgb = hslToRgb(tempHSL[0], tempHSL[1], Math.max(tempHSL[2], 0.8));
 
-      colors[i] = (rgb.red / 255.0);
-      colors[i + 1] = (rgb.green / 255.0);
-      colors[i + 2] = (rgb.blue / 255.0);
+      colors[i] = (rgb[0] / 255.0);
+      colors[i + 1] = (rgb[1] / 255.0);
+      colors[i + 2] = (rgb[2] / 255.0);
     }
 
     return colors;
