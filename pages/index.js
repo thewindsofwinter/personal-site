6b175e79e73651d8c40e98c662fc736a6d0a90ad
Andy Tang <winterwind2022@gmail.com>
2022-12-28 23:03:19 -0600

    mitigate security risk

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,7 @@ export default function Home() {
                 <th className="surname">N</th>
                 <td className="spacing"></td>
                 <td className="logo">
-                  <a target="_blank" href="https://github.com/thewindsofwinter">
+                  <a target="_blank" href="https://github.com/thewindsofwinter" rel="noreferrer">
                     <svg xmlns="http://www.w3.org/2000/svg" className="icon-b icon-tabler icon-tabler-brand-github" width="80%" height="80%" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                         <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
@@ -70,7 +70,7 @@ export default function Home() {
                 <th className="surname">G</th>
                 <td className="spacing"></td>
                 <td className="logo">                  
-                  <a target="_blank" href="weixin://dl/chat?andyytang">
+                  <a target="_blank" href="weixin://dl/chat?andyytang" rel="noreferrer">
                     <svg xmlns="http://www.w3.org/2000/svg" className="icon-t icon-tabler icon-tabler-brand-wechat" width="80%" height="80%" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                       <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                       <path d="M16.5 10c3.038 0 5.5 2.015 5.5 4.5c0 1.397 -.778 2.645 -1.999 3.47l-.001 2.03l-1.964 -1.178a6.649 6.649 0 0 1 -1.536 .178c-3.038 0 -5.5 -2.015 -5.5 -4.5s2.462 -4.5 5.5 -4.5z"></path>
@@ -83,7 +83,7 @@ export default function Home() {
                   </a>
                 </td>
                 <td className="logo">
-                  <a target="_blank" href="https://linkedin.com/in/andyytang">
+                  <a target="_blank" href="https://linkedin.com/in/andyytang" rel="noreferrer">
                     <svg xmlns="http://www.w3.org/2000/svg" className="icon-t icon-tabler icon-tabler-brand-linkedin" width="80%" height="80%" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                       <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                       <rect x="4" y="4" width="16" height="16" rx="2"></rect>
