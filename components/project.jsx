87f20f2cbff93e79f9a499536e80261663e0b608
Andy Tang <winterwind2022@gmail.com>
2022-12-29 17:44:50 -0600

    project compartmentalization and full header

---
+++ components/project.jsx
@@ -0,0 +1,19 @@
+import projectData from '../projects.json';
+
+function Project(props) {
+    let project = projectData[props.project];
+
+    return (
+        <a href={project.link}>
+                <div className="relative w-72 h-96">
+                    <img className="absolute rounded-2xl z-0" src={project.image}></img>
+                    <div className="absolute bottom-0 w-full h-2/3 bg-gray-800 text-white z-5 p-4 rounded-b-2xl">
+                        <h3 className="text-3xl border-b-2">{project.title}</h3>
+                        <p className="mt-4">{project.description}</p>
+                    </div>
+                </div>
+            </a>
+    );
+}
+
+export default Project;
--- pages/projects.js
+++ pages/projects.js
@@ -16,6 +16,20 @@ export default function Projects() {
 
       <div className = "wrapper relative w-screen h-screen z-5 flex flex-col">
         <NavBar logo={true} projects={true} />
+
+        <div className="flex-grow splash flex flex-row flex-wrap justify-center items-center w-4/5 m-auto text-white">
+            <a href="https://github.com/thewindsofwinter/anima">
+                <div className="relative w-72 h-96">
+                    <img className="absolute rounded-2xl z-0" src="../2mo_neural.png"></img>
+                    <div className="absolute bottom-0 w-full h-2/3 bg-gray-800 text-white z-5 p-4 rounded-b-2xl">
+                        <h3 className="text-3xl border-b-2">ANIMA</h3>
+                        <p className="mt-4">I developed a novel graph-based algorithm to quantify the extent of astrogliosis
+                            (brain scarring) in images of immunostained mouse brain slices, improving accuracy and accelerating
+                            data analysis by 98%. Analysis results were published in PNAS 119<em>(51)</em>, DOI: 10.1073/pnas.2021265118.</p>
+                    </div>
+                </div>
+            </a>
+        </div>
       </div>
     </div>
   )
