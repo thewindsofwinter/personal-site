58070df34558430c7c0b8f1b29009933af09e896
Andy Tang <winterwind2022@gmail.com>
2022-12-30 12:35:42 -0600

    add image component for more efficient rendering

--- components/project.jsx
+++ components/project.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import projectData from '../projects.json';
 
 function Badge(props) {
@@ -26,11 +27,17 @@ function Badge(props) {
 
 function Project(props) {
     let project = projectData[props.project];
+    let height = 400 * project.h / project.w;
 
     return (
         <a href={project.link}>
                 <div className="project relative m-4 rounded-2xl hover:shadow-md">
-                    <img className="absolute rounded-2xl z-0" src={project.image}></img>
+                    <Image className="absolute rounded-2xl z-0" 
+                        src={project.image} 
+                        alt={project.alt}
+                        width={400}
+                        height={height}
+                    />
                     <div className="absolute bottom-0 w-full h-2/3 bg-gray-800 text-white z-5 p-4 rounded-b-2xl">
                         <h3 className="block text-3xl border-b-2">{project.title} <Badge type={project.type} /></h3>
                         <p className="text-lg mt-4">{project.description}</p>
