418f8d94224319d02053d5f9bd7b6fcfeda38577
Andy Tang <winterwind2022@gmail.com>
2023-10-07 00:13:17 -0700

    projects rewrite

--- components/project.jsx
+++ components/project.jsx
@@ -2,30 +2,35 @@ import Image from 'next/image'
 import projectData from '../projects.json';
 
 function Badge(props) {
-    let type = props.type;
+    let type = props.type % 10;
+    let classification = (props.type - type) / 10;
+    console.log(classification);
+
+    let classes = ["Internship", "Hackathon", "Research", "Extracurricular", "Personal"];
 
     switch(type) {
         case 0:
             return (
-                <span className="float-right text-sm bg-teal-600 p-1 m-1 mr-0 rounded-lg">Actively Maintained</span>
+                <span className="float-right text-sm bg-teal-600 p-1 m-1 mr-0 rounded-lg">{`Maintaining | ${classes[classification]}`}</span>
             );
         case 1:
             return (
-                <span className="float-right text-sm bg-amber-600 p-1 m-1 mr-0 rounded-lg">Handed to New Maintainer</span>
+                <span className="float-right text-sm bg-amber-600 p-1 m-1 mr-0 rounded-lg">{`Completed | ${classes[classification]}`}</span>
             );
         case 2:
             return (
-                <span className="float-right text-sm bg-lime-600 p-1 m-1 mr-0 rounded-lg">Actively Developing</span>
+                <span className="float-right text-sm bg-lime-600 p-1 m-1 mr-0 rounded-lg">{`Developing | ${classes[classification]}`}</span>
             );
         default:
             return (
-                <span className="float-right text-sm bg-blue-600 p-1 m-1 mr-0 rounded-lg">Planning/Prototyping</span>
+                <span className="float-right text-sm bg-blue-600 p-1 m-1 mr-0 rounded-lg">{`Prototyped | ${classes[classification]}`}</span>
             );
 
     }
 }
 
 function Project(props) {
+    console.log(props);
     let project = projectData[props.project];
     let height = 400 * project.h / project.w;
 
@@ -42,7 +47,7 @@ function Project(props) {
                         height={height}
                     />
                     <div className="absolute bottom-0 w-full h-3/4 bg-gray-800 text-white z-5 p-4 rounded-b-2xl">
-                        <h3 className="block text-2xl border-b-2">{project.title}</h3>
+                        <h3 className="block text-xl pb-2 border-b-2">{project.title}</h3>
                         <Badge type={project.type} />
                         <p className="pt-4 clear-right">{project.description}</p>
                     </div>
@@ -61,7 +66,7 @@ function Project(props) {
                         height={height}
                     />
                     <div className="absolute bottom-0 w-full h-2/3 bg-gray-800 text-white z-5 p-4 rounded-b-2xl">
-                        <h3 className="block text-3xl border-b-2">{project.title} <Badge type={project.type} /></h3>
+                        <h3 className="block text-2xl pb-2 border-b-2">{project.title} <Badge type={project.type} /></h3>
                         <p className="text-lg mt-4">{project.description}</p>
                     </div>
                 </div>
@@ -70,14 +75,12 @@ function Project(props) {
 }
 
 function ProjectList(props) {
+    console.log(Object.keys(projectData))
     return (
         <div className="flex-grow splash flex flex-row flex-wrap justify-center items-center w-4/5 mx-auto mt-8 text-white">
-            <Project project="ANIMA" small={props.small} />
-            <Project project="USNCO" small={props.small} />
-            <Project project="JHMC" small={props.small} />
-            <Project project="Multi24" small={props.small} />
-            <Project project="Azimuth" small={props.small} />
-            <Project project="Fortune-ate" small={props.small} />
+            {Object.keys(projectData).map((project) => (
+                <Project key={project} project={project} small={props.small} />
+            ))}
         </div>
     )
 }
